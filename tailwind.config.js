8e5cec6a2b6dffb934e5145ba53169bca97b6426
shrigharat <gharatshri014@gmail.com>
2025-04-19 23:11:35 +0530

    fixed date sup error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    '**/*.html'
-  ],
+  content: ['**/*.html'],
   theme: {
     extend: {
       gridTemplateRows: {
-        'custom-6': 'repeat(5, 250px)'
+        'custom-6': 'repeat(5, 250px)',
       },
       fontFamily: {
         heading: ['Epilogue', 'sans-serif'], // H1, H2, H3
@@ -14,14 +12,14 @@ module.exports = {
       },
       colors: {
         primary: {
-          DEFAULT: 'var(--primary)'
-        }
-      }
-    }
+          DEFAULT: 'var(--primary)',
+        },
+      },
+    },
   },
   variants: {
-    extend: {}
+    extend: {},
   },
   plugins: [],
-}
+};
 
